ebbb8365f3ef2c52bbc0e4162db32248b3d1a6e1
Evan Johnson <johnsonev207@gmail.com>
2026-04-11 11:34:26 -0700

    Improve cauldron visuals

---
+++ scenes/cauldron/fire_light/fire_light.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var noise_texture: NoiseTexture2D
+
+var _time_passed: float = 0.0
+
+@onready var _sprite_flicker: Sprite2D = $SpriteFlicker
+
+func _process(delta: float) -> void:
+	_time_passed += delta
+	var sampled_noise: float = abs(noise_texture.noise.get_noise_1d(_time_passed))
+	_sprite_flicker.modulate.a = sampled_noise
